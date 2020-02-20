50c113f899dbf87763a3f2783e175b97ef22b5a4
Romualdo Santiago Santiago <rsantiago@inndot.com>
2020-02-20 15:04:41 -0600

    formulario para el login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user';
+import { JwtResponse } from '../models/jwt-response';
+import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class AuthService {
+  AUTH_SERVER: string = 'http://localhost:3000';
+  authSubject = new BehaviorSubject(false);
+  private token: string;
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  register(user: User): Observable<JwtResponse> {
+    return this.httpClient.post<JwtResponse>(`${this.AUTH_SERVER}/register`,
+      user).pipe(tap(
+        (res: JwtResponse) => {
+          if (res) {
+            // guardar token
+            this.saveToken(res.dataUser.accessToken, res.dataUser.expiresIn);
+          }
+        })
+      );
+  }
+
+  login(user: User): Observable<JwtResponse> {
+    return this.httpClient.post<JwtResponse>(`${this.AUTH_SERVER}/login`,
+      user).pipe(tap(
+        (res: JwtResponse) => {
+          if (res) {
+            // guardar token
+            this.saveToken(res.dataUser.accessToken, res.dataUser.expiresIn);
+          }
+        })
+      );
+  }
+
+  logout(): void {
+    this.token = '';
+    localStorage.removeItem("ACCESS_TOKEN");
+    localStorage.removeItem("EXPIRES_IN");
+  }
+
+  private saveToken(token: string, expiresIn: string): void {
+    localStorage.setItem("ACCESS_TOKEN", token);
+    localStorage.setItem("EXPIRES_IN", expiresIn);
+    this.token = token;
+  }
+
+  private getToken(): string {
+    if (!this.token) {
+      this.token = localStorage.getItem("ACCESS_TOKEN");
+    }
+    return this.token;
+  }
+
 }
