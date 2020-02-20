e4020403b7a48b37705412e80a6a8b5996841528
Romualdo Santiago Santiago <rsantiago@inndot.com>
2020-02-20 15:14:36 -0600

    funcionalidad para el registro de usuario

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/user';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
-
+  onRegister(form): void {
+    this.authService.register(form.value).subscribe(res => {
+      this.router.navigateByUrl('/auth');
+    });
+  }
 }
